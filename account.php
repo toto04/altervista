9a64b8c3992fc86c02e57d2a03d7f2cde41e13c2
Tommaso Morganti <toto04.2001@gmail.com>
2019-02-13 22:53:25 +0100

    Aggiunta la funzione di login, che non è che funzioni benissimo, ma funziona

--- account.php
+++ account.php
@@ -13,19 +13,31 @@
 	<div id="contentWrapper">
 		<div id="left"></div>
 		<div id="content">
+			ciao
 			<?php
 			require 'database.php';
 			if ($_POST['sub'] == 'login') {
-				// TODO: gestire il login
+				// TODO: Rendere tutto più bello
+				$usr = $_POST['username'];
+				$pwd = hash('sha256', $_POST['password']);
+
+				$logQuery = "SELECT password FROM users WHERE username='$usr'";
+				$cont = mysql_query($logQuery);
+				$pass = mysql_fetch_array($cont)['password'];
+				if ($pwd == $pass) {
+					$_SESSION['isLogged'] = true;
+					$_SESSION['username'] = $usr;
+				} else {
+					echo "Password sbagliata!";
+				}
 				echo "Log";
-				//$_SESSION['isLogged'] = true;
-				//$_SESSION['username'] = $_POST['username'];
-				//$_SESSION['password'] = $_POST['password'];
+
 			} else if ($_POST['sub'] == 'register') {
 				$usr = $_POST['username'];
 				$email = $_POST['email'];
 				$pwd = hash('sha256', $_POST['password']);
 
+				// TODO: Impedire la creazione di più utenti con lo stesso nome
 				$newUsr = "INSERT INTO users (username, email, password) VALUES ('$usr','$email','$pwd')";
 				mysql_query($newUsr);
 
@@ -38,7 +50,12 @@
 
 			if ($_SESSION['isLogged']) {
 				echo "Logged";
+				echo "<div class='title'><h1>"
+				. "Benvenuto, " . $_SESSION['username']
+				. "! </h1></div>";
+
 				// TODO: Creare interfaccia utente
+				// TODO: creare tasto per uscire
 			} else {
 				//echo "not logged";
 				require './elements/logForms.html';
