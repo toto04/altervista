d593db722d642f884ce0dd176bd6f7549f91f779
Tommaso Morganti <toto04.2001@gmail.com>
2019-02-15 00:34:39 +0100

    La sessione è messa in cima alla pagina per evitare problemi di header

--- head.php
+++ head.php
@@ -3,6 +3,3 @@
 <link rel="stylesheet" href="/src/style.css"></link>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript" src="/src/script.js"></script>
-<?php
-session_start();  // La sessione è costante in tutte le pagine in cui viene importato
-?>
