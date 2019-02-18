e373d59cfea8992f4d9cd1cfc48d7ee30fc4d114
Tommaso Morganti <toto04.2001@gmail.com>
2019-02-18 08:32:35 +0100

    if block per controllare se c'è MySQL

--- database.php
+++ database.php
@@ -1,6 +1,10 @@
 <?php
 $DB_name = "my_tommasomorganti";
-$link = mysql_connect($server, $username, $password);
-mysql_set_charset('utf8');
-$db = mysql_select_db($DB_name, $link) or die("Errore");
+if (function_exists(mysql_connect)) {
+  $link = mysql_connect($server, $username, $password);
+  mysql_set_charset('utf8');
+  $db = mysql_select_db($DB_name, $link) or die("Errore");
+} else {
+  echo "No MySQL Connection";
+}
 ?>
