ae21b9f6452608f0a87f1e728c90104a992cf1d0
Tommaso Morganti <totoscemo@gmail.com>
2019-02-20 23:03:52 +0100

    Recensioni rinnovate con login

--- recensioni.php
+++ recensioni.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
 <head>
@@ -15,10 +16,13 @@
     <div id="content">
       <script src="/src/javascript/review.js" charset="utf-8"></script>
       <?php
-      // TODO: Gestire le recensioni con l'account
+      // TODO: Permettere di eliminare le recensioni
       //Gestisce le informazioni
-      if (isset($_POST["save"])) {
-        $insert = "INSERT INTO my_tommasomorganti.comments (id, Mark, Name, Comment, data) VALUES (NULL," . $_POST["mark"] . ",'" . $_POST["name"] . "','" . $_POST["comment"] . "', CURRENT_TIMESTAMP());";
+      if (isset($_POST["save"]) && $_SESSION['isLogged']) {
+        $mk = $_POST['mark'];
+        $cm = $_POST['comment'];
+        $nm = $_SESSION['username'];
+        $insert = "INSERT INTO my_tommasomorganti.comments (id, Mark, Name, Comment, data) VALUES (NULL, $mk, '$nm', '$cm', CURRENT_TIMESTAMP());";
         mysql_query($insert);
       }
       ?>
@@ -43,18 +47,30 @@
       echo "<h1> Media dei voti: " . $avr . "/10</h1>";
       ?>
       <p>Dammi consigli, opinioni o suggerimenti per migliorare questo sito, dai dettagli ai contenuti, <a href="#" onclick="openReview();">recensisci il sito</a></p>
-      <div id="newReview">
-        <div id="close" onclick="closeReview();"><div></div></div>
-        <form method="post">
-          <label>Nome:</label><br/>
-          <input type="text" name="name" required><br/>
-          <label>Voto:</label><br/>
-          <input type="number" name="mark" min="1" max="10" required><br/>
-          <label>Commento:</label><br/>
-          <textarea id="comment" type="text" name="comment" required></textarea><br/>
-          <button class="submitButton" type="submit" name="save">save</button>
-          <p style="font-size: 10px">Per favore non fate SQL injections, è vulnerabilissimo</p>
-        </form>
+      <div id='newReviewWrapper'>
+        <div id="newReview">
+          <div id="close" onclick="closeReview();"><div></div></div>
+          <?php
+          if ($_SESSION['isLogged']) {
+            echo '
+            <form method="post">
+            <!-- <label>Nome:</label><br/>
+            <input type="text" name="name" required><br/> -->
+            <label>Voto:</label><br/>
+            <input type="number" name="mark" min="1" max="10" required><br/>
+            <label>Commento:</label><br/>
+            <textarea id="comment" type="text" name="comment" required></textarea><br/>
+            <button class="submitButton" type="submit" name="save">save</button>
+            <p style="font-size: 10px">Per favore non fate SQL injections, è vulnerabilissimo</p>
+            </form>';
+          } else {
+            echo "<label>Accedi per postare un commento</label>"
+            . '<button class="submitButton" onclick="loadPage('
+            . "'/account.php'"
+            . ');">Login</button>';
+          }
+          ?>
+        </div>
       </div>
 
       <?php
@@ -62,11 +78,11 @@
         $row = $arr[$i];
         echo
         '<div class="review">
-          <div class="reviewHead">
-            <span>' . $row[2] . ' - ' . $row[1] . '/10</span>
-            <span style="float: right; margin-right: 8px;">' . $row[4] . "</span>
-          </div>
-          <p>" . $row[3] . '</p>
+        <div class="reviewHead">
+        <span>' . $row[2] . ' - ' . $row[1] . '/10</span>
+        <span style="float: right; margin-right: 8px;">' . $row[4] . "</span>
+        </div>
+        <p>" . $row[3] . '</p>
         </div>';
       }
       ?>
