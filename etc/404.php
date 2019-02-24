af05239c49fc7d19f0e0fa5b4be6b1725cbc3985
Tommaso Morganti <totoscemo@gmail.com>
2019-02-24 19:45:14 +0100

    Gestione errore 404

---
+++ etc/404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+<head>
+	<?php require '../head.php'; ?>
+	<script type="text/javascript" id="pageName">
+	var pageName = "Errore 404";
+	</script>
+	<title></title>
+</head>
+<body>
+	<div id="header"></div>
+	<div id="contentShadow" onclick="closeLeft();"></div>
+	<div id="contentWrapper">
+		<div id="left"></div>
+		<div id="content">
+      <div class="title">
+        <h1>Errore 404</h1>
+        <h2>Pagina non trovata</h2>
+        <a href="#" onclick="loadPage('/index')">Torna allo Home</a>
+      </div>
+		</div>
+	</div>
+</body>
+</html>
